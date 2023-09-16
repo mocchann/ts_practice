4357d5635084cffac4f4d119f2fe0ff401206602
もっちゃん <s7056i3056@gmail.com>
2023-09-16 21:44:52 +0900

    test: 検証

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,14 @@
-const secret = process.env.SECRET ?? "default";
+import { createInterface } from "readline";
 
-console.log(`secretは${secret}です`);
+const rl = createInterface({
+    input: process.stdin,
+    output: process.stdout
+});
+
+rl.question('数値打てyo', (line) => {
+    const num = Number(line);
+    const message = 0 <= num && num < 100
+        ? `${num}は0以上100未満だzo`
+        : `${num}は0以上100未満ではないzo`;
+    console.log(message);
+});
