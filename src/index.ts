5685e0217685716d6808a656441238d54716f1dd
もっちゃん <s7056i3056@gmail.com>
2023-09-16 23:41:11 +0900

    test: for文の検証

--- src/index.ts
+++ src/index.ts
@@ -1,14 +1,8 @@
-import { createInterface } from "readline";
-
-const rl = createInterface({
-    input: process.stdin,
-    output: process.stdout
-});
-
-rl.question('数値打てyo', (line) => {
-    const num = Number(line);
-    const message = 0 <= num && num < 100
-        ? `${num}は0以上100未満だzo`
-        : `${num}は0以上100未満ではないzo`;
-    console.log(message);
-});
+let sum = 0;
+for(;;) {
+    sum += 1;
+    if (sum >= 100) {
+        break;
+    }
+}
+console.log(sum);
