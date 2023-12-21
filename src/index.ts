29fd0a2646d95287cf53a1d98e70d3a69520c4f0
もっちゃん <s7056i3056@gmail.com>
2023-12-21 23:38:35 +0900

    再代入しないぱたーん

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,10 @@
 const arr = [1, 2, 4, 8, 16, 32];
 
-const [first, second, third] = arr;
+// 分割代入やらないパターン
+const first = arr[0];
+const second = arr[1];
+const third = arr[2];
+
 console.log(first);
 console.log(second);
 console.log(third);
