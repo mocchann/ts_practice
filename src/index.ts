87befd07175746cad74a63b488dd7612beaa9600
もっちゃん <s7056i3056@gmail.com>
2023-12-22 09:19:58 +0900

    要素をスキップして分割代入

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,8 @@
-const arr = [1, 2, 4, 8, 16, 32];
+const arr = [1,2,3,4,5];
 
-// 分割代入やらないパターン
-const first = arr[0];
-const second = arr[1];
-const third = arr[2];
+// 要素をスキップするパターン
+const [, foo, bar, , baz] = arr;
 
-console.log(first);
-console.log(second);
-console.log(third);
+console.log(foo);
+console.log(bar);
+console.log(baz);
