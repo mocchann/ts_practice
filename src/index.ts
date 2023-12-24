f166ad147e76d38e9908713e06468fc8c08f8ef7
もっちゃん <s7056i3056@gmail.com>
2023-12-24 23:31:00 +0900

    三項演算子で書いたときのコード

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,9 @@
 type Obj = { foo?: number };
-const obj1: Obj = {};
-console.log(obj1.foo);
 
 const obj2: Obj = { foo: -1234 };
-console.log(obj2);
+// console.log(obj2);
 
-const { foo = 500 } = obj1;
-console.log(foo);
+// const { foo: bar = 500 } = obj2;
+// console.log(bar);
 
-const { foo: bar = 500 } = obj2;
-console.log(bar);
+obj2.foo !== undefined ? console.log(obj2.foo) : console.log(500);
