608a049a8e882935688aac8257b9bc816fab080a
もっちゃん <s7056i3056@gmail.com>
2023-12-30 00:06:39 +0900

    配列のrestパターン

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,7 @@
-const obj = {
-  foo: 123,
-  bar: "string",
-  baz: false,
-};
+const arr = [1, 1, 2, 3, 5, 8, 13];
 
-const { foo, ...restObj } = obj;
-
-console.log(foo);
-console.log(restObj);
+const [first, second, third, ...rest] = arr;
+console.log(first);
+console.log(second);
+console.log(third);
+console.log(rest);
