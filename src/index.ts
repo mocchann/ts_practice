1c0464a5bac926f1a7c428ecc68b93f90ee96cf2
もっちゃん <s7056i3056@gmail.com>
2023-12-31 15:42:20 +0900

    現在の日時と時刻が表示

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,2 @@
-const arr = [1, 1, 2, 3, 5, 8, 13];
-
-const [first, second, third, ...rest] = arr;
-console.log(first);
-console.log(second);
-console.log(third);
-console.log(rest);
+const d = new Date();
+console.log(d);
