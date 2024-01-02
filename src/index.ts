6742880cc32e3ae861c16764799f09432efae719
もっちゃん <s7056i3056@gmail.com>
2024-01-02 23:27:53 +0900

    とりあえず動く形にする

--- src/index.ts
+++ src/index.ts
@@ -5,10 +5,29 @@ type User = {
 }
 
 const data: string = `
-  uhyo,26,1
-  John, Smith,17,0
-  Mary Sue,14,1
+uhyo,26,1
+John Smith,17,0
+Mary Sue,14,1
 `;
+
+const newData = data.split("\n").filter((rowData) => rowData !== '');
+const users = [
+  {
+    name: newData[0].split(",")[0],
+    age: newData[0].split(",")[1],
+    premiumUser: newData[0].split(",")[2],
+  },
+  {
+    name: newData[1].split(",")[0],
+    age: newData[1].split(",")[1],
+    premiumUser: newData[1].split(",")[2],
+  },
+  {
+    name: newData[2].split(",")[0],
+    age: newData[2].split(",")[1],
+    premiumUser: newData[2].split(",")[2],
+  },
+];
 
 for (const user of users) {
   if (user.premiumUser) {
