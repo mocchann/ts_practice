ab062615bc8dc6eff2fc272d73202a634997b734
もっちゃん <s7056i3056@gmail.com>
2024-01-02 20:31:09 +0900

    正規表現の例

--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,6 @@
-const d = new Date();
-console.log(d);
+const r = /ab+c/;
+
+console.log(r.test("abbbbc"));
+console.log(r.test("Hello, abc world!"));
+console.log(r.test("ABC"));
+console.log(r.test("こんにちは"));
