92582042991e307716c6a3d7420a85f10391f7e4
もっちゃん <s7056i3056@gmail.com>
2024-01-02 21:34:19 +0900

    TSのオブジェクトは中身が本当にオブジェクトであることを保証しない例

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,6 @@
-type HasLength = {
-  length: number
-};
-
-const obj: HasLength = "foobar";
-
-console.log(obj);
+let val: {} = 123;
+console.log(val);
+val = "foobar";
+console.log(val);
+val = null;
+console.log(val);
