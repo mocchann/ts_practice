780ab349ff70d1eb45f6fb5b1a8edbf128ab7be1
もっちゃん <s7056i3056@gmail.com>
2024-01-02 20:49:34 +0900

    キャプチャリンググループの例

--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,6 @@
-console.log("Hello, abbbbbbc world! abbc".replace(/ab+c/, "foobar"));
-console.log("Hello, abbbbbbc world! abbc".replace(/ab+c/g, "foobar"));
+const result = "Hello, abbbbbbc world! abc".match(/a(b+)c/);
+
+if (result !== null) {
+  console.log(result[0]);
+  console.log(result[1]);
+}
