0315f7d48c2b2984a77947818fa772be954587b5
もっちゃん <s7056i3056@gmail.com>
2024-01-02 21:16:12 +0900

    mapのsetは何回でもできる

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,5 @@
-const map: Map<string, number> = new Map();
-map.set("foo", 1234);
+const map: Map<string, number> = new Map().set("foo", 1234);
+map.set("foo", 12345);
 
 console.log(map.get("foo"));
 console.log(map.get("bar"));
