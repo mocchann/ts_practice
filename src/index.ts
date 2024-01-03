b508d741f36ea06cacc9cd83c9d044cfcfe397fe
もっちゃん <s7056i3056@gmail.com>
2024-01-03 11:12:12 +0900

    返り値のない関数の例

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,12 @@
-console.log(range(5, 10));
-
-function range(min: number, max: number): number[] {
-  const result = [];
-  for(let i = min; i <= max; i++) {
-    result.push(i);
+function helloWorldNTimes(n: number): void {
+  if (n >= 100) {
+    console.log(`${n}回なんて無理です！`);
+    return;
   }
-  return result;
+  for (let i=0; i < n; i++) {
+    console.log("Hello World!");
+  }
 }
+
+helloWorldNTimes(10);
+helloWorldNTimes(1000);
