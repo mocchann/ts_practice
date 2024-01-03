a2976af9748f33c566453b044b3f5ef017b96e78
もっちゃん <s7056i3056@gmail.com>
2024-01-03 12:08:06 +0900

    省略形は{}とreturnを省略できるが、式は1つしかかけない

--- src/index.ts
+++ src/index.ts
@@ -1,15 +1,15 @@
+type Human = {
+  height: number,
+  weight: number,
+};
+
+const calcBMI = ({
+  height, weight
+}: Human): number => weight / height ** 2;
+
 const uhyo: Human = {
   height: 1.84,
   weight: 72
 };
 
 console.log(calcBMI(uhyo));
-
-type Human = {
-  height: number;
-  weight: number;
-};
-
-const calcBMI = function({ height, weight }: Human): number {
-  return weight / height ** 2;
-};
