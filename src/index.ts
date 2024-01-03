4988e3354a2f54b1e2f4d9a5c7d9d2c86e14ee35
もっちゃん <s7056i3056@gmail.com>
2024-01-03 12:37:10 +0900

    メソッド記法と通常の記法＋アロー関数でオブジェクトを定義

--- src/index.ts
+++ src/index.ts
@@ -1,21 +1,9 @@
-type Human = {
-  height: number,
-  weight: number,
+const obj = {
+  double(num: number): number {
+    return num * 2;
+  },
+  double2: (num: number): number => num * 2,
 };
 
-type ReturnObj = {
-  bmi: number,
-};
-
-const calcBMIObject = ({
-  height, weight
-}: Human): ReturnObj => ({
-  bmi: weight / height ** 2
-});
-
-const uhyo: Human = {
-  height: 1.84,
-  weight: 72
-};
-
-console.log(calcBMIObject(uhyo));
+console.log(obj.double(100));
+console.log(obj.double2(-50));
