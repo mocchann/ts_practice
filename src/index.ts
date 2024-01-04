53cfd0e7f56adb6ea8312202d1b51ae5a95e71a8
もっちゃん <s7056i3056@gmail.com>
2024-01-04 14:24:05 +0900

    numsをスプレッド構文で使うためにはタプル型を宣言する

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,4 @@
-const sum = (...args: number[]): number => {
-  let result = 0;
-  for (const num of args) {
-    result += num;
-  }
-  return result;
-};
+const sum3 = (a: number, b: number, c: number) => a+b+c;
+const nums:[number, number, number] = [1,2,3];
 
-const nums = [1, 2, 3, 4, 5];
-console.log(sum(...nums));
+console.log(sum3(...nums));
