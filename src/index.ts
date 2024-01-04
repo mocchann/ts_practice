0ac522de9979be609e96b773c03207c76fc4c08d
もっちゃん <s7056i3056@gmail.com>
2024-01-04 14:13:24 +0900

    スプレッド構文

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,10 @@
-const sum = (base: number, ...args: number[]): number => {
-  let result = base * 1000;
+const sum = (...args: number[]): number => {
+  let result = 0;
   for (const num of args) {
     result += num;
   }
   return result;
 };
 
-console.log(sum(1, 10, 100));
-console.log(sum(123, 456));
-console.log(sum());
+const nums = [1, 2, 3, 4, 5];
+console.log(sum(...nums));
