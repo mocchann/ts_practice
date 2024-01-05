ed0715529cf4a6afa76ed094824accfd64dc3feb
もっちゃん <s7056i3056@gmail.com>
2024-01-05 23:57:00 +0900

    コールバック関数の逆方向の型推論

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,3 @@
-type F = (arg: number) => string;
-
-const xRepeat: F = (num) => "x".repeat(num);
+const nums = [1,2,3,4,5,6,7,8,9];
+const arr2 = nums.filter((x) => x % 3 === 0);
+console.log(arr2);
