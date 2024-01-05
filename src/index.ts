4a67ebc280b98b09142f6bd8de8da28edd9df9ae
もっちゃん <s7056i3056@gmail.com>
2024-01-06 00:03:17 +0900

    オブジェクトの引数の型推論

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,8 @@
-const nums = [1,2,3,4,5,6,7,8,9];
-const arr2 = nums.filter((x) => x % 3 === 0);
-console.log(arr2);
+type Greetable = {
+  greet: (str: string) => string;
+}
+const obj: Greetable = {
+  greet: (str) => `Hello, ${str}!`
+}
+
+console.log(obj.greet("world"));
