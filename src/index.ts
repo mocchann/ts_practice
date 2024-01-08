cc6cf9fa579704d0040c77a5b181021b6f49fe64
もっちゃん <s7056i3056@gmail.com>
2024-01-08 16:05:54 +0900

    メソッド記法もメソッド名のあと・引数リストの前に
    おく

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,12 @@
-const repeat = <T>(element: T, length: number): T[] => {
-  const result: T[] = [];
-  for (let i = 0; i < length; i++) {
-    result.push(element);
+const utils = {
+    repeat<T>(element: T, length: number): T[] {
+    const result: T[] = [];
+    for (let i = 0; i < length; i++) {
+      result.push(element);
+    }
+    return result;
   }
-  return result;
 }
 
-console.log(repeat<string>("a", 5));
-console.log(repeat<boolean>(true, 3));
+console.log(utils.repeat<string>("a", 5));
+console.log(utils.repeat<boolean>(true, 3));
