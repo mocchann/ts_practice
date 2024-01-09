6d8e867d99632e33a01503b141a76c732b74ad2c
もっちゃん <s7056i3056@gmail.com>
2024-01-09 23:56:16 +0900

    class式の実装例

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,16 @@
-class User {
-  constructor(public name: string, private age: number) {}
+const User = class {
+  name: string;
+  age: number;
+
+  constructor(name: string, age: number) {
+    this.name = name;
+    this.age = age;
+  }
 
   public isAdult(): boolean {
     return this.age >= 20;
   }
-}
+};
 
 const uhyo = new User("uhyo", 26);
 console.log(uhyo.name);
