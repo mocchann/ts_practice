b6d61515a1f83995e6047dc5bd1a6635eec67bc6
もっちゃん <s7056i3056@gmail.com>
2024-01-09 23:07:41 +0900

    アクセシビリティ修飾詞を使ってコンストラクタを省略した形で書く

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,5 @@
 class User {
-  name: string;
-  private age: number;
-
-  constructor(name: string, age: number) {
-    this.name = name;
-    this.age = age;
-  }
+  constructor(public name: string, private age: number) {}
 
   public isAdult(): boolean {
     return this.age >= 20;
@@ -15,4 +9,3 @@ class User {
 const uhyo = new User("uhyo", 26);
 console.log(uhyo.name);
 console.log(uhyo.isAdult());
-console.log(uhyo.age);
