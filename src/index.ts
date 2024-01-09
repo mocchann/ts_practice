39f3e0e036e0ebd167aac5eaf11524674cdbbc1f
もっちゃん <s7056i3056@gmail.com>
2024-01-09 22:00:30 +0900

    コンストラクタを定義すればclassプロパティの初期値は外部から受け取れる

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,11 @@
 class User {
-  name: string = "";
-  age: number = 0;
+  name: string;
+  age: number;
+
+  constructor(name: string, age: number) {
+    this.name = name;
+    this.age = age;
+  }
 
   isAdult(): boolean {
     return this.age >= 20;
@@ -11,9 +16,6 @@ class User {
   }
 }
 
-const uhyo = new User;
-console.log(uhyo.isAdult());
-
-uhyo.setAge(26);
-console.log(uhyo.age);
+const uhyo = new User("uhyo", 26);
+console.log(uhyo.name);
 console.log(uhyo.isAdult());
