e665977bb3696a399c9005da31688483ab2b464e
もっちゃん <s7056i3056@gmail.com>
2024-01-09 23:04:07 +0900

    privateメソッドの例

--- src/index.ts
+++ src/index.ts
@@ -1,27 +1,18 @@
 class User {
-  static adminName: string = "uhyo";
-  static getAdminUser() {
-    return new User(User.adminName, 26);
-  }
-
   name: string;
-  age: number;
+  private age: number;
 
   constructor(name: string, age: number) {
     this.name = name;
     this.age = age;
   }
 
-  isAdult(): boolean {
+  public isAdult(): boolean {
     return this.age >= 20;
   }
 }
 
-console.log(User.adminName);
-const admin = User.getAdminUser();
-console.log(admin);
-console.log(admin.name);
-console.log(admin.age);
-
 const uhyo = new User("uhyo", 26);
-console.log(uhyo.adminName);
+console.log(uhyo.name);
+console.log(uhyo.isAdult());
+console.log(uhyo.age);
