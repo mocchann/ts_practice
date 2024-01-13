d2160e47ee327acfa54634220bd4aeb9013c2593
もっちゃん <s7056i3056@gmail.com>
2024-01-13 16:21:21 +0900

    関数ないのthisは呼び出され方によって決まる

--- src/index.ts
+++ src/index.ts
@@ -1,19 +1,11 @@
-class User {
-  name: string;
-  #age: number;
-
-  constructor(name: string, age: number) {
-    this.name = name;
-    this.#age = age;
+const user = {
+  name: "uhyo",
+  age: 26,
+  isAdult() {
+    return this.age >= 20;
   }
+};
 
-  public isAdult(): boolean {
-    return this.#age >= 20;
-  }
-}
-
-
-const uhyo = new User("uhyo", 26);
-
-const isAdult = uhyo.isAdult;
-console.log(isAdult());
+console.log(user.isAdult());
+user.age = 15;
+console.log(user.isAdult());
