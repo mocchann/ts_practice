2b66fbf00f028c979f0d9e47153f3d521ce02427
もっちゃん <s7056i3056@gmail.com>
2024-01-14 00:16:15 +0900

    Arrayはクラスであるため、継承ができる

--- src/index.ts
+++ src/index.ts
@@ -1,15 +1,18 @@
-const arr1 = new Array<number>();
-console.log(arr1);
-console.log(typeof arr1);
+class RepeatArray<T> extends Array<T> {
+  repeat(times: number): RepeatArray<T> {
+    const result = new RepeatArray<T>();
+    for (let i = 0; i < times; i++) {
+      result.push(...this);
+      console.log(result);
+    }
+    return result;
+  }
+}
 
-const arr2: number[] = [];
-console.log(arr2);
-console.log(typeof arr2);
+const arr = new RepeatArray(1, 2);
 
-const arr3 = new Array(1, 3, 57);
-console.log(arr3);
-console.log(typeof arr3);
+arr.push(3);
 
-const arr4 = new Array(10);
-console.log(arr4);
-console.log(typeof arr4);
+const repeated = arr.repeat(3);
+
+console.log(repeated);
