cb694348e3314478f4dc3fc02d87da40ba62a135
もっちゃん <s7056i3056@gmail.com>
2024-01-14 17:34:17 +0900

    プライベートプロパティにすればカプセル化できる

--- src/index.ts
+++ src/index.ts
@@ -1,18 +1,18 @@
 class User {
-  name: string;
-  age: number;
+  readonly #name: string;
+  readonly #age: number;
 
   constructor(name: string, age: number) {
     if (name === "") {
       throw new Error("名前はからにできない");
     } else {
-      this.name = name;
-      this.age = age;
+      this.#name = name;
+      this.#age = age;
     }
   }
 
   public getMessage(message: string): string {
-    return `${this.name} (${this.age}) 「${message}」`;
+    return `${this.#name} (${this.#age}) 「${message}」`;
   }
 }
 
