3ddbdf73d74475ec9388440f205346fc22060b41
もっちゃん <s7056i3056@gmail.com>
2024-01-14 15:45:31 +0900

    例外処理を何もしない場合、引数を省略できる

--- src/index.ts
+++ src/index.ts
@@ -1,14 +1,5 @@
 try {
-  console.log("エラーを発生させる");
-  throwError();
-  console.log("エラーを発生させた");
-} catch (err) {
-  console.log("エラーをキャッチした");
-  console.log(err);
-}
-console.log("おわり");
-
-function throwError() {
-  const error = new Error("エラーが発生！！！！！");
-  throw error;
+  someFunc();
+} catch {
+  // 何もしない
 }
