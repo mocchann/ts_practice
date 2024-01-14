43debde955f60c775ceec74910db115d5acda238
もっちゃん <s7056i3056@gmail.com>
2024-01-14 16:48:12 +0900

    finallyは例外処理以外の割り込みにも使うことができる。returnを返す前に割り込ますこともできる

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,14 @@
-try {
-  console.log("エラーを発生させる");
-  throwError();
-  console.log("エラーを発生させた");
-} finally {
-  console.log("finallyブロック");
-}
-console.log("try分の後ろ");
+console.log(sum(100));
 
-function throwError() {
-  throw new Error("エラーが発生!!!");
+function sum(max: number): number {
+  try {
+    let result = 0;
+    for (let i = 0; i <= max; i++) {
+      result += i;
+    }
+    return result;
+  } finally {
+    console.log("sumから脱出する");
+  }
+  
 }
