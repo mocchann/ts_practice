d66528ce36ee6fa91493493f30ac5031cb94bbe3
もっちゃん <s7056i3056@gmail.com>
2024-01-19 00:08:51 +0900

    擬似的代数データ型の宣言に合わないtagはエラー

--- src/index.ts
+++ src/index.ts
@@ -9,3 +9,18 @@ type Human = {
 }
 
 type User = Animal | Human;
+
+const tama: User = {
+  tag: "animal",
+  species: "fFFFFF",
+};
+
+const uhyo: User = {
+  tag: "human",
+  name: "uhyo",
+};
+
+const alien: User = {
+  tag: "alien",
+  name: "gray",
+};
