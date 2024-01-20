8973ddabfaf7a14fe08b28d6abd238311e63b56c
もっちゃん <s7056i3056@gmail.com>
2024-01-20 15:07:47 +0900

    as constは宣言以降で要素が変化することがない

--- src/index.ts
+++ src/index.ts
@@ -1,18 +1,3 @@
-type Animal = {
-  tag: "animal";
-  species: string;
-};
-type Human = {
-  tag: "human";
-  name: string;
-};
+const names = ["uhyo", "john", "taro"];
 
-type User = Human | Animal;
-
-function getNamesIfAllHuman(users: readonly User[]): string[] | undefined {
-  if (users.every(user => user.tag === "human")) {
-    // everyでHuman[]への絞り込みを行ったがコンパイラが認識しない、かつ、この処理のあとにusersにAnimalをpushすることもないためasでHuman[]を指定している
-    return (users as Human[]).map(user => user.name);
-  }
-  return undefined;
-}
+const namess = ["uhyo", "john", "taro"] as const;
