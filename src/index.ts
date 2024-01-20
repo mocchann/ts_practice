61cddc464384bcd2719ecf9b62e3f11ecde8ce31
もっちゃん <s7056i3056@gmail.com>
2024-01-20 12:20:15 +0900

    asを使う場合はその使い方が正しいのか読んだ人がわかるようにコメントを残しておく

--- src/index.ts
+++ src/index.ts
@@ -11,6 +11,7 @@ type User = Human | Animal;
 
 function getNamesIfAllHuman(users: readonly User[]): string[] | undefined {
   if (users.every(user => user.tag === "human")) {
+    // everyでHuman[]への絞り込みを行ったがコンパイラが認識しない、かつ、この処理のあとにusersにAnimalをpushすることもないためasでHuman[]を指定している
     return (users as Human[]).map(user => user.name);
   }
   return undefined;
