62bd8e600d70641f952dd318e8fde29db0e04344
もっちゃん <s7056i3056@gmail.com>
2024-01-27 10:46:35 +0900

    seerts isは例外処理との親和性が高い

--- src/index.ts
+++ src/index.ts
@@ -4,11 +4,20 @@ type Human = {
   age: number;
 };
 
-function isHuman(value: any): value is Human {
-  if (value == null) return false;
-  return (
-    value.type === "Human" &&
-    typeof value.name === "string" &&
-    typeof value.age === "number"
-  );
+function assertHuman(value: any): asserts value is Human {
+  if (value == null) {
+    throw new Error('Given value is null or undefined');
+  }
+  if (
+    value.type !== "Human" ||
+    typeof value.name !== "string" ||
+    typeof value.age !== "number"
+  ) {
+    throw new Error('Given value is not a Human');
+  }
+}
+
+function checkAndUseHuman(value: unknown) {
+  assertHuman(value);
+  const name = value.name;
 }
