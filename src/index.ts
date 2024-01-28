0de69fb1edd58aef49ec5530e8619ae3170f9181
もっちゃん <s7056i3056@gmail.com>
2024-01-28 11:24:17 +0900

    ...配列型はタプル型の中で1回しか使えない

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,4 @@
-type NumberAndStrings = [number, ...string[], number];
+type NumberAndStrings = [number, ...string[], number, ...number[]];
 
 const arr1: NumberAndStrings = [25, "uhyo", "hyo", "hyo", 0];
 const arr2: NumberAndStrings = [25, 25];
