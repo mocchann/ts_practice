b980afc05173517be095487d8ee897e02ad04076
もっちゃん <s7056i3056@gmail.com>
2024-01-28 11:18:36 +0900

    ...配列型をタプル型の最初や真ん中に含めることもできる

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,4 @@
-type NumberAndStrings = [number, ...string[]];
+type NumberAndStrings = [number, ...string[], number];
 
-const arr1: NumberAndStrings = [25, "uhyo", "hyo", "hyo"];
-const arr2: NumberAndStrings = [25];
-
-const arr3: NumberAndStrings = ["uhyo", "hyo"];
-const arr4: NumberAndStrings = [25, 26, 27];
-const arr5: NumberAndStrings = [];
+const arr1: NumberAndStrings = [25, "uhyo", "hyo", "hyo", 0];
+const arr2: NumberAndStrings = [25, 25];
