2a898c001ae1f3bc4642e526db385819c4ee11f7
もっちゃん <s7056i3056@gmail.com>
2024-01-31 00:32:34 +0900

    importとexportの実装例

--- src/index.ts
+++ src/index.ts
@@ -1,32 +1,3 @@
-type Some<T> = {
-  tag: "some";
-  value: T;
-}
-type None = {
-  tag: "none";
-}
+import { name, age } from "./uhyo.js";
 
-type Option<T> = Some<T> | None;
-
-function mapOption<T, U>(obj: Option<T>, callback: (value: T) => U): Option<U> {
-  switch (obj.tag) {
-    case "some":
-      return {
-        tag: "some",
-        value: callback(obj.value)
-      }
-    case "none":
-      return {
-        tag: "none",
-      }
-  }
-}
-
-function dOption(obj: Option<number>) {
-  return mapOption(obj, x => x * 2);
-}
-const four: Option<number> = { tag: "some", value: 4 };
-const nothing: Option<number> = { tag: "none" };
-
-console.log(dOption(four));
-console.log(dOption(nothing));
+console.log(name, age);
