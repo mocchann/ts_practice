7b832f5d4134c439d7a68b90ab24e2d9880726c4
もっちゃん <s7056i3056@gmail.com>
2024-02-04 15:27:25 +0900

    マッチ対象が存在しない場合は0を返すようにした

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,5 @@
 import { readFile } from "fs";
 
 readFile('../uhyo.txt', 'utf8', (err, file) => {
-  console.log(file.match(/uhyo/g)?.length);
+  console.log((file.match(/uhyo/g) || []).length);
 });
