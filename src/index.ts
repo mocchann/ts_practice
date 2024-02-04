2e9f41cbbba996a84edd74f9eeaa8e2b332ad742
もっちゃん <s7056i3056@gmail.com>
2024-02-04 22:14:11 +0900

    promiseのthenは複数登録可能であり、登録された順番に実行される

--- src/index.ts
+++ src/index.ts
@@ -2,6 +2,12 @@ import { readFile } from "fs/promises";
 
 const p = readFile("uhyo.txt", "utf8");
 
-p.then((data) => {
-  console.log(data);
+p.then((result) => {
+  console.log(1);
+});
+p.then((result) => {
+  console.log(2);
+});
+p.then(() => {
+  console.log(3);
 });
