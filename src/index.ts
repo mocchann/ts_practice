585dc6d2424a50d84187b6ead7b998366a7ef813
もっちゃん <s7056i3056@gmail.com>
2024-02-06 23:16:26 +0900

    Promise.raceを使って、5秒スリープと処理を競争させれば5秒応答がないとタイムアウトさせるような実装も可能

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,18 @@
 import { readFile } from "fs/promises";
 
+const sleepReject = (duration: number) => {
+  return new Promise<never>((resolve, reject) => {
+    setTimeout(resolve, duration);
+  });
+}
+
 const p = Promise.race([
   readFile("foo.txt", "utf8"),
-  readFile("bar.txt", "utf8"),
-  readFile("baz.txt", "utf8"),
+  sleepReject(5000),
 ]);
 
 p.then((result) => {
-  console.log(result);
+  console.log("成功", result);
+}, (error: unknown) => {
+  console.log("失敗", error);
 });
