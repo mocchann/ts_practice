8bd45f932847f92b9a543c9c95b146d2fa3ec2e8
もっちゃん <s7056i3056@gmail.com>
2024-02-06 22:55:50 +0900

    Promise.allはすべて成功したときだけ成功、どれかひとつでも失敗すると失敗

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,13 @@
-const p = Promise.resolve();
+import { readFile } from "fs/promises";
 
-p.then(() => {
-  console.log(2);
+const pFoo = readFile("foo.txt", "utf8");
+const pBar = readFile("bar.txt", "utf8");
+const pBaz = readFile("baz.txt", "utf8");
+
+const p = Promise.all([pFoo, pBar, pBaz]);
+
+p.then((result) => {
+  console.log("foo.txt", result[0]);
+  console.log("bar.txt", result[1]);
+  console.log("baz.txt", result[2]);
 });
-
-console.log(1);
