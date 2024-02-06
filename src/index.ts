56350a5fabab9589f39c5413d3dc74d4d8e78176
もっちゃん <s7056i3056@gmail.com>
2024-02-06 23:38:45 +0900

    Promise.allSettledは成功・失敗いずれも成功とみなすため、両方の処理が終了するまで実行する

--- src/index.ts
+++ src/index.ts
@@ -2,17 +2,15 @@ import { readFile } from "fs/promises";
 
 const sleepReject = (duration: number) => {
   return new Promise<never>((resolve, reject) => {
-    setTimeout(resolve, duration);
+    setTimeout(reject, duration);
   });
-}
+};
 
-const p = Promise.race([
+const p = Promise.allSettled([
   readFile("foo.txt", "utf8"),
   sleepReject(5000),
 ]);
 
 p.then((result) => {
-  console.log("成功", result);
-}, (error: unknown) => {
-  console.log("失敗", error);
+  console.log(result);
 });
