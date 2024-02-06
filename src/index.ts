a0a77453da8527fe7247dc1b3d89beaf57d088ac
もっちゃん <s7056i3056@gmail.com>
2024-02-06 23:10:16 +0900

    Promise.raceは引数に渡す非同期処理の中で一番最初の結果を全体の結果とする

--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,11 @@
 import { readFile } from "fs/promises";
 
-const p = Promise.all([
+const p = Promise.race([
   readFile("foo.txt", "utf8"),
   readFile("bar.txt", "utf8"),
   readFile("baz.txt", "utf8"),
 ]);
 
-p.then(([foo, bar, baz]) => {
-  console.log("foo.txt", foo);
-  console.log("bar.txt", bar);
-  console.log("baz.txt", baz);
+p.then((result) => {
+  console.log(result);
 });
