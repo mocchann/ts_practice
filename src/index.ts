ffd819094d31d5d8992d03c8b00427428e1deecf
もっちゃん <s7056i3056@gmail.com>
2024-02-11 22:56:42 +0900

    アロー関数の{}をつけてreturnできてなかったので直した

--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,12 @@
 import { readFile } from "fs/promises";
 
-const repeat10 = (str: string) => {
+const repeat10 = (str: string) =>
   new Promise<string>((resolve) => {
     setTimeout(
       () => resolve(str.repeat(10)),
       1000
     );
   });
-}
 
 readFile("foo.txt", "utf8")
   .then((result) => repeat10(result))
