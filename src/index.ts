563fef02136694edef4bf62643a71ad87953943f
もっちゃん <s7056i3056@gmail.com>
2024-02-12 11:13:09 +0900

    awaitには基本Promiseオブジェクトを与える

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,15 @@
-async function get3(): number {
+const sleep = (duration: number) => {
+  return new Promise<void>((resolve) => {
+    setTimeout(resolve, duration);
+  });
+};
+
+async function get3() {
+  await sleep(1000);
   return 3;
 }
+
+const p = get3();
+p.then(num => {
+  console.log(num);
+});
