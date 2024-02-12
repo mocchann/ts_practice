b91b7033042caf6d4d0d9d09d042dde3806238de
もっちゃん <s7056i3056@gmail.com>
2024-02-12 13:37:37 +0900

    readFileを使って実装

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,23 @@
-import { readFile, writeFile } from "fs/promises"
+import { readFile } from "fs/promises";
+import path from "path";
+import { fileURLToPath } from "url";
 
-const main = async () => {
-  const fooContent = await readFile("foo.txt", "utf8");
-  await writeFile("bar.txt", fooContent + fooContent);
-  console.log("書き込み完了");
+const filePath = fileURLToPath(import.meta.url);
+const fileDir = path.dirname(filePath);
+const dataFile = path.join(fileDir, "../uhyo.txt");
+
+const data = await readFile(dataFile, { encoding: "utf8" });
+
+let count = 0;
+let currentIndex = 0;
+while (true) {
+  const nextIndex = data.indexOf("uhyo", currentIndex);
+  if (nextIndex >= 0) {
+    count++;
+    currentIndex = nextIndex + 1;
+  } else {
+    break;
+  }
 }
+
+console.log(count);
