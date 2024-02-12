f00ebf78dcf8eddefc98db52fcf4fd48daba9971
もっちゃん <s7056i3056@gmail.com>
2024-02-12 19:46:27 +0900

    1秒間で非同期処理が終われば次へ進み、終わらなければプロセス終了する実装

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,20 @@
 import { readFile } from "fs/promises";
-import path from "path";
+import path, { resolve } from "path";
 import { fileURLToPath } from "url";
+
+const sleep = (duration: number) => {
+  return new Promise<void>((resolve) => {
+    setTimeout(resolve, duration);
+  });
+}
 
 const filePath = fileURLToPath(import.meta.url);
 const fileDir = path.dirname(filePath);
 const dataFile = path.join(fileDir, "../uhyo.txt");
+
+sleep(1).then(() => {
+  process.exit();
+});
 
 const data = await readFile(dataFile, { encoding: "utf8" });
 
