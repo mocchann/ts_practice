114497af1666b1343aee0a044bbe48787640df44
もっちゃん <s7056i3056@gmail.com>
2024-02-12 12:14:26 +0900

    async アロー関数の例

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,6 @@
-import { readFile, writeFile } from "fs/promises";
+import { readFile, writeFile } from "fs/promises"
 
-const main = async function () {
+const main = async () => {
   const fooContent = await readFile("foo.txt", "utf8");
   await writeFile("bar.txt", fooContent + fooContent);
   console.log("書き込み完了");
