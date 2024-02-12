b4f9e2891ad7ef3aadd86ec90d510f4e2fc2cfe8
もっちゃん <s7056i3056@gmail.com>
2024-02-12 10:36:01 +0900

    async関数は非同期的に動作するので、同期処理に割り込みはできず、同期処理が終了したあとに結果が返却される

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,11 @@
 async function get3(): Promise<number> {
+  console.log("呼び出された");
   return 3;
 }
+
+console.log("呼び出す");
+const p = get3();
+p.then(num => {
+  console.log(num);
+})
+console.log("呼び出した");
