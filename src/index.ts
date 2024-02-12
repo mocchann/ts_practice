77c804a2cfcdecbbcc9dde55c4905398b2ebc5ca
もっちゃん <s7056i3056@gmail.com>
2024-02-12 10:17:36 +0900

    async関数の構文

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,3 @@
-import("fs/promises")
-  .then(({ readFile }) =>
-    readFile("fooo.txt", "utf8")
-  )
-  .then((result) => {
-    console.log(result);
-  })
-  .catch((error) => {
-    console.log("エラー", error);
-  })
+async function get3(): Promise<number> {
+  return 3;
+}
