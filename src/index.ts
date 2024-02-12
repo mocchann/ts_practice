274b3462494065e1306a3e54e70b457add77aa8d
もっちゃん <s7056i3056@gmail.com>
2024-02-12 11:22:23 +0900

    awaitにPromiseを与えれば、非同期処理の実行を中断できる

--- src/index.ts
+++ src/index.ts
@@ -5,11 +5,15 @@ const sleep = (duration: number) => {
 };
 
 async function get3() {
+  console.log("呼び出された");
   await sleep(1000);
+  console.log("次の処理に進む");
   return 3;
 }
 
+console.log("呼び出す");
 const p = get3();
 p.then(num => {
   console.log(num);
 });
+console.log("呼び出した");
